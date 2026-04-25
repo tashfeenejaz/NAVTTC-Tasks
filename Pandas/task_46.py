f5cd1a6ce530a7b0b8239b3fc4cdb6dd89a7efb2
tashfeenejaz <tashfeenejaz18@gmail.com>
2026-04-26 00:47:49 +0500

    Pandas Data analyzation

---
+++ Pandas/task_46.py
@@ -0,0 +1,15 @@
+#Get a quick overview by printing the first 10 rows of the DataFrame
+import pandas as pd
+
+df = pd.read_csv('iris_multi_class.csv')
+print(df.head(10))
+
+#Print the first 5 rows of the DataFrame
+df = pd.read_csv('data.csv')
+print(df.head())
+
+#Print the last 5 rows of the DataFrame
+print(df.tail())
+
+#Print information about the data
+print(df.info())
