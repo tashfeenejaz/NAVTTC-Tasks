8e40c2e9998c3ef268f121c2a6964ecf2393c108
tashfeenejaz <tashfeenejaz18@gmail.com>
2026-04-25 23:06:53 +0500

    Numpy Array sorting

---
+++ Numpy/task_39.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#Sort the array
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+#Sort the array alphabetically
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+#Sort a boolean array
+arr = np.array([True, False, True])
+print(np.sort(arr))
+
+#Sort a 2-D array
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+print(np.sort(arr))
