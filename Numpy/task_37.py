750ce9bc3c992fbc3c45ffd103e8e3bec314b6bd
tashfeenejaz <tashfeenejaz18@gmail.com>
2026-04-25 22:59:02 +0500

    Numpy Array splitting

---
+++ Numpy/task_37.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+#Split the array in 3 parts
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+#Split the array in 4 parts
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 4)
+print(newarr)
+
+#Access the splitted arrays
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 3)
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+#Split the 2-D array into three 2-D arrays
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+#Split the 2-D array into three 2-D arrays
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+#Split the 2-D array into three 2-D arrays along columns
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3, axis=1)
+print(newarr)
+
+#Use the hsplit() method to split the 2-D array into three 2-D arrays along columns
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.hsplit(arr, 3)
+print(ewarr)
