d1ad2e4a4d2697b57f6aa6c075416b71a963b877
tashfeenejaz <tashfeenejaz18@gmail.com>
2026-04-25 18:13:14 +0500

    Conversion from celsius to Fahrenheit

---
+++ Python/task_07.py
@@ -0,0 +1,8 @@
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print(f"{celsius} degree Celsius is equal to : {fahrenheit:.3f} degree Fahrenheit")
