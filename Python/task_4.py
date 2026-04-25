9ad62b75f0d4f61322ed596784ccb1ec7de6b229
Tashfeen Ejaz <tashfeenejaz18@gmail.com>
2026-04-25 18:09:00 +0500

    Delete Python/task_4.py

--- Python/task_4.py
+++
@@ -1,9 +0,0 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print(f"The sum of {num1} and {num2} is {sum}")
