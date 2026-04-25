9c6d0df258d71520d0d9cd617c83cd7c7c95a23e
tashfeenejaz <tashfeenejaz18@gmail.com>
2026-04-26 01:17:06 +0500

    Finding standard deviation & variance

---
+++ Stats/task_50.py
@@ -0,0 +1,10 @@
+#finding the standard deviation
+import numpy as np
+speed = [86,87,88,86,87,85,86]
+x = np.std(speed)
+print(x)
+
+# finding the variance
+speed = [32,111,138,28,59,77,97]
+y = np.var(speed)
+print(y)
